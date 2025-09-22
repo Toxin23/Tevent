102b1413dc063e32216053fbd3ba83071bc2c797
Toxin23 <63064322+Toxin23@users.noreply.github.com>
2025-09-22 12:34:43 +0200

    Update TicketManager.php

--- src/TicketManager.php
+++ src/TicketManager.php
@@ -1,12 +1,21 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-use Endroid\QrCode\QrCode;
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Writer\PngWriter;
 
 class TicketManager {
     public static function generate($event, $name) {
         $data = $event . '|' . $name . '|' . uniqid();
-        $qr = new QrCode($data);
-        return $qr->writeDataUri();
+
+        $result = Builder::create()
+            ->writer(new PngWriter())
+            ->data($data)
+            ->size(300)
+            ->margin(10)
+            ->build();
+
+        return 'data:image/png;base64,' . base64_encode($result->getString());
     }
 }
 ?>
